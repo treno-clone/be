053ff2a0d6f0713fe6f94810772a980d344a52d8
LmNguyen <65638661+LmNgn@users.noreply.github.com>
2026-01-05 21:09:32 +0700

    config: add dotenv config and connect db

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from "express"
+import connectDB from "./src/config/connectDB"
 //tạo ra 1 server express mang tên app
 const app = express()
 // cho phép nhận vào body dạng JSON, nếu không có thì body dạng JSON sẽ thành undefined
 // app.use -> middleware dùng cho tất cả các request
 app.use(express.json())
-
+connectDB()
 app.listen(3000, () => {
     console.log(`Server is running on http://localhost:3000`)
 })
---
+++ src/config/dotenvConfig.js
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config({
+    // Nếu có nhiều môi trường và muốn chọn env cụ thể
+    // path: [],
+    // utf 8 là mặc định, cho nodejs biết đọc file bằng bảng mã nào
+    // encoding: "utf8", 
+    // cho phép .env ghi đè biến môi trường có sẵn
+    // override: true
+})
+
+export const {
+    HOST, PORT, DB_URL, JWT_SECRET, JWT_EXPIRE_IN
+} = process.env
